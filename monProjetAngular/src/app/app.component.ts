21311f07ea9334c9370c7c5d25e995a8e06d24ea
Administrator <admin@example.com>
2020-11-17 23:13:50 +0100

    pipe async with promise

--- monProjetAngular/src/app/app.component.ts
+++ monProjetAngular/src/app/app.component.ts
@@ -5,21 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  isAuth : boolean  = false ;
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date() ;
+      setTimeout(() => {
+        resolve(date)
+      }, 2000
+      )
+    }
+  );
+  
+  isAuth: boolean = false;
   appareils = [
-    {name:"machine à laver",status:"allumé"},
-    {name:"télévision",status:"allumé"},
-    {name:"oridinateur",status:"éteint"}
-]
-
-  onAllumer(){
-    console.log("allumer tout") ;
+    { name: 'machine à laver', status: 'allumé' },
+    { name: 'télévision', status: 'allumé' },
+    { name: 'ordinateur', status: 'éteint' }
+  ];
+  
+  onAllumer() {
+    console.log('allumer tout');
   }
-
-  constructor(){
-    setTimeout(()=>{
+  
+  constructor() {
+    setTimeout(() => {
       this.isAuth = true;
-    }, 4000)
+    }, 4000);
   }
 }
